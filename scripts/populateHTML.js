f13c0e0d714e3ccbe815f801dca88a333b8af1ec
Brittney Corona <BrittneyLCorona@gmail.com>
2015-08-13 13:30:16 -0500

    changes

--- scripts/populateHTML.js
+++ scripts/populateHTML.js
@@ -1,20 +1,5 @@
-define(["jquery", "hbs", "dom-access"], function($, Handlebars, D){
-  return {
-    putSeenMoviesInHTML: function(data) {
-        require(['hbs!../templates/moviesSeen'],function(movieTemplate){
-          D.moviesSeenDiv.html(movieTemplate(data));
-        });
-      },
-      putToSeeMoviesInHTML: function(data) {
-        require(['hbs!../templates/moviesToSee'],function(movieTemplate){
-          D.moviesToSee.html(movieTemplate(data));
-        });
-      },
-      putSearchInHTML: function(data) {
-        require(['hbs!../templates/addMovie'],function(movieTemplate){
-          D.moviesToAdd.html(movieTemplate(data));
-        });
-      }      
+define(["jquery", "hbs"], function($, Handlebars){
+  return {     
     };
   }
 ); 
