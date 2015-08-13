33a83e68de21475cc199a6a26e367842a4edc811
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-08-13 13:29:40 -0500

    only 5 search results

--- scripts/populateHTML.js
+++ scripts/populateHTML.js
@@ -1,20 +1,6 @@
-define(["jquery", "hbs", "dom-access"], function($, Handlebars, D){
+define(["jquery", "hbs"], function($, Handlebars){
   return {
-    putSeenMoviesInHTML: function(data) {
-        require(['hbs!../templates/moviesSeen'],function(movieTemplate){
-          D.moviesSeenDiv.html(movieTemplate(data));
-        });
-      },
-      putToSeeMoviesInHTML: function(data) {
-        require(['hbs!../templates/moviesToSee'],function(movieTemplate){
-          D.moviesToSee.html(movieTemplate(data));
-        });
-      },
-      putSearchInHTML: function(data) {
-        require(['hbs!../templates/addMovie'],function(movieTemplate){
-          D.moviesToAdd.html(movieTemplate(data));
-        });
-      }      
+    
     };
   }
 ); 
